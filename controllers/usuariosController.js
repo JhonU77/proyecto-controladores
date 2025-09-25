dbcdd3efa93d532f8968959d5ad908111ab2cd6a
Dev A <devA@gmail.com>
2025-09-25 12:46:28 -0500

    feat: agregar controlador de usuarios

---
+++ controllers/usuariosController.js
@@ -0,0 +1,6 @@
+export const getUsuarios = (req, res) => {
+  res.json({ message: "Listado de usuarios" });
+};
+export const createUsuario = (req, res) => {
+  res.json({ message: "Usuario creado" });
+};
