3da5da6cf20307a33f315c89082c47bd851a713d
Dev B <devB@example.com>
2025-09-25 12:59:45 -0500

    feat: agregar controlador de productos

---
+++ controllers/productosController.js
@@ -0,0 +1,7 @@
+export const getProductos = (req, res) => {
+  res.json({ message: "Listado de productos" });
+};
+
+export const createProducto = (req, res) => {
+  res.json({ message: "Producto creado" });
+};
